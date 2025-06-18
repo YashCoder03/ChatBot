7df5bba3e04294c685f434745a7146353cbd4446
Yash Deshmukh <yashdeshmukh.college@gmail.com>
2025-06-18 13:22:37 +0530

    removed unwanted consoles

--- Backend/sockets/chat.socket.js
+++ Backend/sockets/chat.socket.js
@@ -4,7 +4,7 @@ exports.handleSocketConnection = (socket) => {
     console.log('✅ A client connected:', socket.id);
   socket.on("user_message", async (data) => {
     const message  = data;
-    console.log("message is =>" + data);
+    // console.log("message is =>" + data);
 
     if (!message) {
       socket.emit("bot_message", { reply: "Message is required!" });
@@ -13,7 +13,7 @@ exports.handleSocketConnection = (socket) => {
 
     try {
       const result = await mistral.invoke(message);
-      console.log(result.content);
+      // console.log(result.content);
       socket.emit("bot_message", { reply: result.content });
     } catch (error) {
       console.error("MistralAI Error:", error);
