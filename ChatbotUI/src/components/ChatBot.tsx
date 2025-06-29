0f74425ab2976bc976c630d8ed2c010c82b3b550
Tejas Palwe <tejaspalwe6@gmail.com>
2025-06-30 00:11:05 +0530

    styling and UI fixes

--- ChatbotUI/src/components/ChatBot.tsx
+++ ChatbotUI/src/components/ChatBot.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { SendHorizonal, Paperclip } from 'lucide-react';
 import { io } from 'socket.io-client';
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
 
 const URL = "https://chatbot-production-2562.up.railway.app";
 
@@ -9,14 +11,35 @@ const socket = io(URL, {
 });
 
 export default function Chatbot() {
+
+  const [isDark, setIsDark] = useState(
+    () => localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
   const [messages, setMessages] = useState([
     { sender: 'bot', text: 'Hello! How can I help you today?' },
   ]);
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
   const chatEndRef = useRef<any>(null);
+
   useEffect(() => {
-    socket.on('bot_message', (data:any) => {
-        console.log(data)
+    const root = window.document.documentElement;
+    if (isDark) {
+      root.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      root.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [isDark]);
+
+  useEffect(() => {
+    socket.on('bot_message', (data: any) => {
+      if (!socket.connected) {
+        alert('App Crashed');
+      }
+      setLoading(false);
       setMessages((prev) => [...prev, { sender: 'bot', text: data.reply }]);
     });
 
@@ -29,6 +52,7 @@ export default function Chatbot() {
     if (!input.trim()) return;
     setMessages((prev) => [...prev, { sender: 'user', text: input }]);
     socket.emit('user_message', input);
+    setLoading(true);
     setInput('');
   };
 
@@ -41,21 +65,20 @@ export default function Chatbot() {
     if (file && file.type === 'application/pdf') {
       setMessages((prev) => [...prev, { sender: 'user', text: `📄 Sent file: ${file.name}` }]);
 
-       const formData = new FormData();
-        formData.append('pdf', file);
-        formData.append("socketId", socket.id ?? "");
+      const formData = new FormData();
+      formData.append('pdf', file);
+      formData.append("socketId", socket.id ?? "");
 
-        try {
-          await fetch(`${URL}/api/upload`, {
-            method: 'POST',
-            body: formData,
-          });
+      try {
+        await fetch(`${URL}/api/upload`, {
+          method: 'POST',
+          body: formData,
+        });
 
-          // const data = await res.json();
-          console.log('✅ PDF uploaded:');
-    } catch (err) {
-      console.error('❌ Upload failed', err);
-    }
+        console.log('✅ PDF uploaded:');
+      } catch (err) {
+        console.error('❌ Upload failed', err);
+      }
     } else {
       alert('Only PDF files are allowed!');
     }
@@ -66,38 +89,54 @@ export default function Chatbot() {
   }, [messages]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+    <div className="flex items-center justify-center min-h-screen bg-[#eee0e0] dark:bg-gray-800">
       <div className="flex flex-col w-full max-w-3xl h-[700px] border border-gray-700 rounded-2xl shadow-lg bg-gray-800 overflow-hidden">
-        <div className="p-4 font-bold text-lg bg-blue-700 text-white">Chatbot</div>
 
-        <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-gray-900 text-white">
+        <div className="p-4 font-bold text-lg bg-orange-700 text-white flex justify-between">
+          <div>
+            Chatbot
+          </div>
+          <button
+            onClick={() => setIsDark(!isDark)}
+          >
+            {isDark ? <MdOutlineLightMode />
+              : <MdOutlineDarkMode />}
+          </button>
+        </div>
+
+        <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-stone-100 dark:bg-gray-900 text-white">
           {messages.map((msg, idx) => (
+
             <div
               key={idx}
               className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
             >
               <div
-                className={`px-4 py-2 rounded-2xl max-w-[70%] break-words ${
-                  msg.sender === 'user'
-                    ? 'bg-blue-600 text-white'
-                    : 'bg-gray-700 text-white'
-                }`}
+                className={`px-4 py-2 rounded-2xl max-w-[70%] break-words ${msg.sender === 'user'
+                  ? 'bg-orange-600 text-white '
+                  : `bg-${isDark ? 'gray-700'  : '[#867d7d]'} text-white`
+                  }`}
               >
                 {msg.text}
               </div>
             </div>
           ))}
+          {
+            loading && (
+              !isDark ? <div className="loaderBlack"></div> : <div className="loaderWhite"></div>
+            )
+          }
           <div ref={chatEndRef} />
         </div>
 
-        <div className="p-4 border-t border-gray-700 bg-gray-800 flex gap-2 items-center">
+        <div className="pb-4 px-2 border-gray-700 bg-stone-100 dark:bg-gray-900 flex gap-2 items-center">
           <input
             type="text"
             placeholder="Type your message..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={handleKeyDown}
-            className="flex-1 px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-1 px-4 py-2 bg-white text:black dark:bg-gray-700 dark:text-white border border-gray-600 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           {/* Hidden file input */}
           <label className="cursor-pointer">
@@ -114,7 +153,7 @@ export default function Chatbot() {
 
           <button
             onClick={sendMessage}
-            className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-full"
+            className="bg-orange-600 hover:bg-blue-700 text-white p-2 rounded-full"
           >
             <SendHorizonal size={20} />
           </button>
