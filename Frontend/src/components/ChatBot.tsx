2b81d74108245e9027b51b8925fec35dfa79c852
Tejas Palwe <tejaspalwe6@gmail.com>
2025-07-02 02:05:10 +0530

    add sidebar and styling fixes

--- Frontend/src/components/ChatBot.tsx
+++ Frontend/src/components/ChatBot.tsx
@@ -3,6 +3,7 @@ import { SendHorizonal, Paperclip } from 'lucide-react';
 import { io } from 'socket.io-client';
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
+import SideBar from './SideBar';
 
 const URL = "https://chatbot-production-2562.up.railway.app";
 
@@ -89,8 +90,9 @@ export default function Chatbot() {
   }, [messages]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#eee0e0] dark:bg-gray-800">
-      <div className="flex flex-col w-full max-w-3xl h-[700px] border border-gray-700 rounded-2xl shadow-lg bg-gray-800 overflow-hidden">
+    <div className="flex items-center justify-between min-h-screen bg-[#eee0e0] dark:bg-gray-800">
+      <SideBar/>
+      <div className="flex flex-col w-full h-[700px] border border-gray-700 shadow-lg bg-[#eee0e0] dark:bg-gray-800 overflow-hidden">
 
         <div className="p-4 font-bold text-lg bg-orange-700 text-white flex justify-between">
           <div>
@@ -103,18 +105,17 @@ export default function Chatbot() {
               : <MdOutlineDarkMode />}
           </button>
         </div>
-
-        <div className="flex-1 overflow-y-auto p-4 space-y-3 bg-stone-100 dark:bg-gray-900 text-white">
+        <div className="flex-1 w-full md:w-2/3 m-auto overflow-y-auto p-4 space-y-3 bg-stone-100 dark:bg-gray-900 text-white">
           {messages.map((msg, idx) => (
 
             <div
               key={idx}
-              className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
+              className={`flex  ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
             >
               <div
-                className={`px-4 py-2 rounded-2xl max-w-[70%] break-words ${msg.sender === 'user'
+                className={` px-4 py-2 rounded-2xl max-w-[70%] break-words ${msg.sender === 'user'
                   ? 'bg-orange-600 text-white '
-                  : `${isDark ? 'bg-gray-700'  : 'bg-[#867d7d]'} text-white`
+                  : `${isDark ? 'bg-gray-700' : 'bg-[#867d7d]'} text-white`
                   }`}
               >
                 {msg.text}
@@ -128,8 +129,7 @@ export default function Chatbot() {
           }
           <div ref={chatEndRef} />
         </div>
-
-        <div className="pb-4 px-2 border-gray-700 bg-stone-100 dark:bg-gray-900 flex gap-2 items-center">
+        <div className="pb-4 px-2 w-full md:w-2/3 m-auto border-gray-700 bg-stone-100 dark:bg-gray-900 flex gap-2 items-center">
           <input
             type="text"
             placeholder="Type your message..."
